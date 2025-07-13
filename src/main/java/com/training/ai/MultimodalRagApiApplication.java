d01fef862bc14a18b428eee62873c2752b3494b4
Pimenova, Marina <mnfpma@gmail.com>
2025-07-13 15:49:38 +0300

    Multimodal RAG: Init

---
+++ src/main/java/com/training/ai/MultimodalRagApiApplication.java
@@ -0,0 +1,13 @@
+package com.training.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultimodalRagApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultimodalRagApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/ai/MultimodalRagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultimodalRagApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
